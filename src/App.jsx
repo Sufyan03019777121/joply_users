86b4f2d50a7ad90d908731da7f9bd2bde9823e2b
Sufyan Ali <sufyanpakpatttan@mail.com>
2025-11-10 01:09:50 +0500

    249

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import PublicRoutes from "./routes/PublicRoutes";
 import AuthRoutes from "./routes/AuthRoutes";
 
