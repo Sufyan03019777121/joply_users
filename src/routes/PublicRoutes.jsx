6d21787660d6e14d1657675c1eee0c6f9e65f415
Sufyan Ali <sufyanpakpatttan@mail.com>
2025-11-10 03:25:28 +0500

    253

--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Profile from "../pages/profile/Profile";
+import ServicesOverview from "../pages/servicesOverview/ServicesOverview";
 
 const About = () => <h2 style={{ textAlign: "center", marginTop: 40 }}>ℹ️ About Us</h2>;
-const Services = () => <h2 style={{ textAlign: "center", marginTop: 40 }}>🛠 Our Services</h2>;
+
 const Contact = () => <h2 style={{ textAlign: "center", marginTop: 40 }}>📞 Contact Us</h2>;
 const Jobs = () => <h2 style={{ textAlign: "center", marginTop: 40 }}>💼 Job View</h2>;
 const RegisterJob = () => <h2 style={{ textAlign: "center", marginTop: 40 }}>📝 Register for Job</h2>;
@@ -18,7 +19,7 @@ function PublicRoutes() {
         
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
+        <Route path="/services" element={<ServicesOverview />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/jobs" element={<Jobs />} />
         <Route path="/register-job" element={<RegisterJob />} />
