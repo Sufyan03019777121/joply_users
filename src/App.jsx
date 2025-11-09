cd7b6e78a86d116da5eb973a6cbcba03dc1bc2b5
Sufyan Ali <sufyanpakpatttan@mail.com>
2025-11-10 01:58:00 +0500

    250

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import PublicRoutes from "./routes/PublicRoutes";
 import AuthRoutes from "./routes/AuthRoutes";
 
 function App() {
   return (
     <Router>
-      <HashRouter>
+      <Routes>
         {/* Auth Pages */}
         <Route path="/*" element={<AuthRoutes />} />
 
         {/* Public Pages */}
         <Route path="/app/*" element={<PublicRoutes />} />
-      </HashRouter>
+      </Routes>
     </Router>
   );
 }
